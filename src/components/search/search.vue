ea0b1787239b2bd1385040b92e03e2b9718bb22b
Geng&l <1933292552@qq.com>
2018-08-23 23:19:35 +0800

    search组件生命周期结束时，将query清空

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -65,6 +65,9 @@
         created() {
             this._getHotkey()
         },
+        destroyed() {       //组件生命周期结束时，将query清空
+            this.query = ''
+        },
         methods: {
             back() {
                 this.$router.back()
