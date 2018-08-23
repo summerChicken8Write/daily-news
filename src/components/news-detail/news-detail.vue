5ec8f3f46210f16eba5b1011c8c81f2474ab385f
Geng&l <1933292552@qq.com>
2018-08-23 21:28:13 +0800

    给news-detail设置样式

--- src/components/news-detail/news-detail.vue
+++ src/components/news-detail/news-detail.vue
@@ -3,8 +3,11 @@
         <div class="news-detail">
             <scroll :data="newsData" class="news-content">
                 <div>
-                    <div class="news-heder">
-                        <i class="back" @click="back">back</i>
+                    <div class="news-header">
+                        <div class="top">
+                            <i class="back icon icon-arrow_lift" @click="back"></i>
+                            <h1 class="top-title">每日新闻</h1>
+                        </div>
                         <h1 class="title">{{this.news.title}}</h1>
                     </div>
                     <div class="desc">
@@ -12,9 +15,12 @@
                             <img :src="mediaInfo.avatar_url" alt="" class="avatar">
                             <h2 class="source">{{mediaInfo.screen_name}}</h2>
                         </div>
-                        <div class="comments">{{newsData.comment_count}}评论</div>
-                        <div class="keyWords" v-for="item in keyWords" v-show="keyWords.length">
-                            <span class="item">{{item}}</span>
+                        <div class="comments">{{this.news.datetime}}&nbsp&nbsp&nbsp{{newsData.comment_count}}评论</div>
+                        <div class="key-words" v-show="keyWords.length">
+                            <i class="icon icon-price-tags"></i>
+                            <ul class="tag">
+                                <li class="item" v-for="item in keyWords">{{item}}</li>
+                            </ul>
                         </div>
                     </div>
                     <div class="content" v-html="newsData.content"></div>
@@ -63,6 +69,7 @@
                 }
                 getNewsDetail(this.news.group_id).then((res) => {
                     this.newsData = res.data
+                    console.log(this.news.datetime)
                     this.mediaInfo = res.data.media_user
                     if (this.news.keywords) {
                         this.keyWords = this._normalizeKeyWords(this.news.keywords)
@@ -79,7 +86,8 @@
     }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
+    // 去掉scoped，使样式可以应用到v-html内容中
     @import "~common/stylus/variable"
 
     .slider-enter-active, .slider-leave-active
@@ -97,5 +105,66 @@
         .news-content
             height: 100%
             overflow: hidden
+            .news-header
+                .top
+                    position relative
+                    height 60px
+                    line-height 60px
+                    text-align center
+                    background $color-background
+                    color $color-title
+                    .back
+                        position absolute
+                        top 22px
+                        left 22px
+                        font-size $font-size-title
+                    .top-title
+                        display inline-block
+                        font-size $font-size-title-l
+                .title
+                    margin 30px 15px
+                    font-size $font-size-title-l
+                    font-weight 700
+            .desc
+                margin 30px 15px
+                .media
+                    display inline-block
+                    height 25px
+                    .avatar
+                        width 25px
+                        height 25px
+                        border-radius 50%
+                    .source
+                        display inline-block
+                        vertical-align top
+                        height 25px
+                        line-height 25px
+                .comments
+                    display inline-block
+                    float right
+                    height 25px
+                    line-height 25px
+                    color $color-desc
+                    font-size $font-size-small
+                .key-words
+                    margin-top 10px
+                    font-size $font-size-small
+                    .tag   
+                        margin-left 5px
+                        display inline-block
+                        .item
+                            margin-right 5px
+                            display inline-block
+                            color $color-desc
+            .content
+                padding 30px 15px
+                box-sizing border-box
+                width 100%
+                font-size $font-size-title
+                line-height 1.5
+                p
+                    margin-bottom 20px
+                img
+                    width 100%
 </style>
 
